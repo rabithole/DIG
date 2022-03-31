c0c478e2375ce88570448802c126de40cab6a0e3
Robert Kulp <pedalinon@gmail.com>
2022-03-31 12:23:10 -0700

    accessing prices for single kit of several on same page

--- assets/custom.js
+++ assets/custom.js
@@ -33,62 +33,9 @@
 //  *   bubbles: true
 //  * }));
 //  */
-//     // Adding products to the cart and refreshing the page.
-    
-//     let formBuildKit = document.getElementById('kit-form');
-//     let formValue = document.getElementById('value');
-//     let id = document.getElementsByClassName('product-model'); // id of each product placed in the product title h2 element.
-//     let idArr = [];
+//    
 
-//         // Add each product id to the id array. ( idArr )
-//     for(let i = 0; i < id.length; i++) {
-//         idArr.push(id[i].id );
-//     }
 
-//     console.log('ID Array:--', idArr)
-//     let prodData = idArr.map(id => {
-//         return {
-//             quantity: 1,
-//             id: id
-//         }
-//     })
 
-//     let data = {
-//         items: prodData
-//     }
 
-// function idSubmit() {
-//     console.log('submit')
-//     fetch('/cart/add.js', {
-//         body: JSON.stringify(data),
-//         credentials: 'same-origin',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'X-Requested-With': 'XMLHttpRequest'
-//         },
-//         method: 'POST'
-//     }).then((response) => {
-//         console.log('response', response.json());
-//         window.location.reload();
-//         response.json();
-//     }).then(data => {
-//         console.log("Success", data)
-//         console.log("One more")
-//     }).catch((err) => {
-//         console.log('error');
-//         console.error(err)
-//     });
-// }
-
-// function findSku() {
-//     var productJS = {{ prod.variants | json }}
-//     let prodSku = {{ sku }}
-//     console.log('Product from JS: ', productJS[0].sku, prodSku)
-
-//     for(let i = 0; i < productJS.length; i++ ){
-//         if(productJS[i].sku == prodSku ){
-//             console.log('sku', productJS[i].sku)
-//         }
-//     }
-// }
 
